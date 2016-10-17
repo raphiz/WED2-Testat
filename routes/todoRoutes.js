0ec8c19a5d5b2688619d50e92d51fdbb62e4f292
Raphael Zimmermann <mail@raphael.li>
2016-10-17 15:39:10 +0200

    Clean up routes file

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -3,17 +3,8 @@ var todoController = require('../controller/todoController');
 
 var router = express.Router();
 
-router.get("/", function(req, res){
-    todoController.listDialog(req,res);
-});
-
-router.get("/:id", function(req, res){
-    todoController.tododialog(req,res);
-});
-
-router.post("/create", function(req, res){
-    console.log('?');
-    todoController.create(req,res);
-});
+router.get("/", todoController.listDialog);
+router.get("/:id", todoController.tododialog(req,res));
+router.post("/create", todoController.create(req,res));
 
 module.exports = router;
