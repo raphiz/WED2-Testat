aa3b1ed27084e3271ca8d8a6199030c923d06132
Raphael Zimmermann <mail@raphael.li>
2016-10-25 08:07:21 +0200

    Fix syntax error

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -4,7 +4,7 @@ var todoController = require('../controller/todoController');
 var router = express.Router();
 
 router.get("/", todoController.listDialog);
-router.get("/:id", todoController.tododialog(req,res));
-router.post("/create", todoController.create(req,res));
+router.get("/:id", todoController.tododialog);
+router.post("/create", todoController.create);
 
 module.exports = router;
