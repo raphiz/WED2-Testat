ccf841d7697155356fe17285650c3fce584cc78d
Raphael Zimmermann <mail@raphael.li>
2016-11-13 14:48:20 +0100

    Use template strings for startup message

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(function(err, req, res, next) {
 const hostname = '127.0.0.1';
 const port = 3001;
 app.listen(port, hostname, () => {
-    console.log("Server running at http://${hostname}:${port}/");
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
